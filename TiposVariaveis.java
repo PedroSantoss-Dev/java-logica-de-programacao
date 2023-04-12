1b51dcad64f56d7d18b5cd82e6fad296d089a487
Pedro Santos <90013568+PedroSantoss-Dev@users.noreply.github.com>
2023-04-12 10:00:51 -0300

    feat: add tipos e variaveis

---
+++ TiposVariaveis.java
@@ -0,0 +1,17 @@
+// convertendo um tipo maior em um tipo de tamanho menor
+// double-> float-> long-> int-> char-> short->byte
+
+
+// convertendo um tipo menor em um tamanho maior
+// byte-> short-> char-> int-> long-> float->double
+
+public class TiposVariaveis {
+    public class TiposVariaveis {
+        public static void main(String[] args) throws Exception {
+          byte numero1 = 100;
+          short  numero2=numero1;
+          numero1 = (byte) numero2;
+          System.out.println(numero1);
+        }
+    }
+}
